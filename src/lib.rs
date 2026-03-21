db5bd4726b2807ae4b2ac6af807f332bb7f9ce6f
Nicole LeGare <dlegare.1001@gmail.com>
2026-03-21 00:07:43 -0700

    Add begin steps and some concept of events

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,69 @@
+pub struct State {
+    pub players: Vec<()>,
+
+    /// The current player, i.e. whose turn it is.
+    pub player: PlayerId,
+
+    /// Phase of the current turn.
+    pub phase: Phase,
+
+    pub stack: Vec<()>,
+}
+
+impl State {
+    pub fn tick(&mut self) -> Vec<Event> {
+        match &mut self.phase {
+            Phase::Begin(step) => match step {
+                BeginStep::Untap => {
+                    *step = BeginStep::Upkeep;
+                    return vec![Event::Phase, Event::DayNight, Event::SelectUntap];
+                }
+                BeginStep::Upkeep => {
+                    *step = BeginStep::Draw;
+                }
+                BeginStep::Draw => {
+                    self.phase = Phase::PreCombat;
+                }
+            },
+            Phase::PreCombat => {
+                self.phase = Phase::Combat;
+            }
+            Phase::Combat => {
+                self.phase = Phase::PostCombat;
+            }
+            Phase::PostCombat => {
+                self.phase = Phase::End;
+            }
+            Phase::End => {
+                self.player = (self.player + 1) % self.players.len();
+                self.phase = Phase::Begin(BeginStep::Untap);
+            }
+        }
+
+        vec![]
+    }
+}
+
+type PlayerId = usize;
+
 pub enum Phase {
-    Begin,
+    Begin(BeginStep),
     PreCombat,
     Combat,
     PostCombat,
     End,
+}
+
+pub enum BeginStep {
+    Untap,
+    Upkeep,
+    Draw,
+}
+
+#[derive(Debug)]
+pub enum Event {
+    // Untap events.
+    Phase,
+    DayNight,
+    SelectUntap,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-use mtg_engine::Phase;
+use mtg_engine::{Phase, State};
 
 fn main() {
-    let players = [(), ()];
+    let mut state = State {
+        players: vec![(), ()],
+        player: 0,
+        phase: Phase::Begin(mtg_engine::BeginStep::Untap),
+        stack: vec![],
+    };
 
-    let mut current = 0;
-    let mut phase = Phase::Begin;
     loop {
-        match phase {
-            Phase::Begin => {
-                phase = Phase::PreCombat;
-            }
-            Phase::PreCombat => {
-                phase = Phase::Combat;
-            }
-            Phase::Combat => {
-                phase = Phase::PostCombat;
-            }
-            Phase::PostCombat => {
-                phase = Phase::End;
-            }
-            Phase::End => {
-                current = (current + 1) % players.len();
-                phase = Phase::Begin;
-            }
+        let events = state.tick();
+        for event in events {
+            dbg!(event);
         }
     }
 }
